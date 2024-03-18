442b1dadfe30f75c3408709d2ecae8dd1242d18f
Germain-L <germain.leignel@gmail.com>
2024-03-18 11:26:07 +0100

    Add launch configuration and dependencies for Tauri dev and build*** ***Update Cargo.toml with rusqlite dependency*** ***Update +page.svelte to display formatted duration

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,41 +1,41 @@
 <script lang="ts">
-    import { invoke } from '@tauri-apps/api/tauri';
-    import { onMount } from 'svelte';
+	import { invoke } from '@tauri-apps/api/tauri';
+	import { onMount } from 'svelte';
 
-    let activeWindowText: string = "No active window";
-    let timeSpent: object = {};
+	let activeWindowText: string = 'No active window';
+	let timeSpent: string = "";
 
-    interface ActiveWindow {
-        app_name: string;
-    }
+	interface ActiveWindow {
+		app_name: string;
+	}
 
-    async function fetchActiveWindowInfo(): Promise<void> {
-        try {
-            const activeWindow: ActiveWindow = await invoke('get_active_window_info');
-            activeWindowText = activeWindow.app_name;
+	async function fetchActiveWindowInfo(): Promise<void> {
+		try {
+			const activeWindow: ActiveWindow = await invoke('get_active_window_info');
+			activeWindowText = activeWindow.app_name;
 
-            const time: number = await invoke('get_current_window_time');
+			const time: number = await invoke('get_current_window_time');
             timeSpent = formatDuration(time);
-        } catch (error) {
-            console.error('Error fetching active window info:', error);
-        }
-    }
+		} catch (error) {
+			console.error('Error fetching active window info:', error);
+		}
+	}
 
-    function formatDuration(duration: number): any {
-        // Convert duration in milliseconds to a readable format
-        // Implement this function based on how you want to format the duration
-        // For example, convert to hours, minutes, and seconds
-        // return `${duration}ms`; // Placeholder, replace with actual formatting
-        return duration
-    }
+	function formatDuration(duration: number): string {
+		const seconds = Math.floor((duration / 1000) % 60);
+		const minutes = Math.floor((duration / (1000 * 60)) % 60);
+		const hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
 
-    onMount(() => {
-        const intervalId = setInterval(fetchActiveWindowInfo, 1000);
-        return () => clearInterval(intervalId); // cleanup on component unmount
-    });
+		return `${hours}h ${minutes}m ${seconds}s`;
+	}
+
+	onMount(() => {
+		const intervalId = setInterval(fetchActiveWindowInfo, 1000);
+		return () => clearInterval(intervalId); // cleanup on component unmount
+	});
 </script>
 
 <button on:click={fetchActiveWindowInfo}>Fetch active window info</button>
 
 <p>Active Window: {activeWindowText}</p>
-<p>Time Spent: {JSON.stringify(timeSpent)}</p>
+<p>Time Spent: {timeSpent}</p>
